d82ea5848db626df62b40277b2998905af7e8844
yoomster <nperenboom@hotmail.com>
2024-04-21 13:23:22 +0200

    Added the needed tables

---
+++ HotelDB/dbo/Table/Bookings.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Bookings]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RoomId] INT NOT NULL, 
+    [GuestId] INT NOT NULL, 
+    [StartDate] DATETIME2 NOT NULL, 
+    [EndData] DATETIME2 NOT NULL, 
+    [CheckedIn] BIT NOT NULL DEFAULT 0, 
+    [TotalCost] FLOAT NOT NULL, 
+    CONSTRAINT [FK_Bookings_RoomId] FOREIGN KEY (RoomId) REFERENCES Rooms(Id),
+    CONSTRAINT [FK_Bookings_GuestId] FOREIGN KEY (GuestId) REFERENCES Guests(Id)
+)
