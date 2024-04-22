d345e73cb7b0006967e8394ee44e128d826e7e93
yoomster <nperenboom@hotmail.com>
2024-04-22 11:43:45 +0200

    Added models

---
+++ HotelLibrary/BookingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLibrary;
+
+public record BookingModel(
+     int Id, 
+     RoomModel Room,
+     GuestModel guest,
+     DateOnly StartDate,
+     DateOnly EndDate,
+     bool CheckedIn,
+     int TotalCost
+);
---
+++ HotelLibrary/GuestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLibrary;
+
+public record GuestModel (
+    int Id, 
+    string FirstName, 
+    string LastName
+    );
