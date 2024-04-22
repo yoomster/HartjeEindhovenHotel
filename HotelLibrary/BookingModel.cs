a46b230846090706976140b96dd4ff2c5ff2a6b0
yoomster <nperenboom@hotmail.com>
2024-04-22 15:58:52 +0200

    changed to properties

--- HotelLibrary/BookingModel.cs
+++ HotelLibrary/BookingModel.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace HotelLibrary;
 
-public record BookingModel(
-     int Id, 
-     RoomModel RoomId,
-     GuestModel GuestId,
-     DateOnly StartDate,
-     DateOnly EndDate,
-     bool CheckedIn,
-     float TotalCost
-);
+public record BookingModel
+{
+    public int Id { get; set; }
+    public RoomModel RoomId { get; set; }
+    public GuestModel GuestId { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public bool CheckedIn { get; set; }
+    public float TotalCost { get; set; }
+
+
+    //    int Id, 
+    //     RoomModel RoomId,
+    //     GuestModel GuestId,
+    //     DateOnly StartDate,
+    //     DateOnly EndDate,
+    //     bool CheckedIn,
+    //     float TotalCost
+}
