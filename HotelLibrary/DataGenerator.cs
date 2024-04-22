bce3a4dd6174fe14cbf2f6d56fded057109ac64b
yoomster <nperenboom@hotmail.com>
2024-04-22 13:43:31 +0200

    Added Bogus for creating fake data

---
+++ HotelLibrary/DataGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLibrary;
+
+public class DataGenerator
+{
+    //Faker<RoomTypeModel> roomTypeFake;
+    //public DataGenerator()
+    //{
+    //    Randomizer.Seed = new Random(123);
+
+    //    roomTypeFake = new Faker<RoomTypeModel>()
+    //        .RuleFor(u => u.Title, f => f.Name.Random.String())
+    //        .RuleFor(u => u.Price, f => f.Random.Float(30, 250))
+    //        .RuleFor(u => u.Description, f => f.Lorem);
+    //}
+
+    //public RoomTypeModel GenerateRoomType()
+    //{
+    //    return roomTypeFake.Generate();
+    //}
+
+    Faker<GuestModel> GuestFaker;
+
+    public DataGenerator()
+    {
+        Randomizer.Seed = new Random(123);
+
+        GuestFaker = new Faker<GuestModel>()
+            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+            .RuleFor(u => u.LastName, f => f.Name.LastName())
+            .RuleFor(u => u.StreetAddress, f => f.Address.StreetAddress())
+            .RuleFor(u => u.City, f => f.Address.City())
+            .RuleFor(u => u.Zipcode, f => f.Address.ZipCode());
+    }
+
+    public GuestModel GenerateGuest()
+    {
+        return GuestFaker.Generate();
+    }
+}
--- HotelLibrary/GuestModel.cs
+++ HotelLibrary/GuestModel.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace HotelLibrary;
 
-public record GuestModel (
-    int Id, 
-    string FirstName, 
-    string LastName
-    );
+public record GuestModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string Zipcode { get; set; }
+}
+
+
+    //(
+    //int Id, 
+    //string FirstName, 
+    //string LastName,
+    //string StreetAddress,
+    //string City,
+    //string Zipcode
+    //);
